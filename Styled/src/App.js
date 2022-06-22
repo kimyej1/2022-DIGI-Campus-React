6d30931d7d2a354ffe77031e0ce2d610dcd9b66f
kimyej1 <yeji2501@gmail.com>
2022-06-22 09:11:20 +0900

    2022.6.22 09:11

---
+++ Styled/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+    return (
+        <View>
+            <Text>
+                기본형태 
+            </Text>   
+        </View>
+    );
+};
+
+export default App;
